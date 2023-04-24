fa213dc627c178aff8751038c7b59000050e160f
Coniferish <johnjennings702@gmail.com>
2023-04-24 14:52:06 -0500

    Change mapping to accept generic types instead of hashmap

--- src/main/java/mettle/MappingUtility.java
+++ src/main/java/mettle/MappingUtility.java
@@ -1,22 +1,31 @@
 package mettle;
 
-import java.util.HashMap;
+import java.util.*;
 
 
-public class MappingUtility {
+public class MappingUtility <T> {
+
+    private Map<T, T> mapping;
+
+    public MappingUtility(Map<T, T> initialMapping) {
+        mapping = new HashMap<>();
+        if (initialMapping != null) {
+            mapping.putAll(initialMapping);
+        }
+    }
 
     /**
      * Applies a mapping to an input array based on a given HashMap.
      *
      * @param input The input array to be mapped.
-     * @param map The HashMap representing the mapping.
      * @return The output array with mapped values.
      */
-    public static int[] applyMapping(int[] input, HashMap<Integer, Integer> map) {
-        int[] output = new int[input.length];
+    public T[] applyMapping(T[] input) {
+        T[] output = Arrays.copyOf(input, input.length);
+
         for (int i = 0; i < input.length; i++) {
-            if (map.containsKey(input[i])) {
-                output[i] = map.get(input[i]);
+            if (mapping.containsKey(input[i])) {
+                output[i] = mapping.get(input[i]);
             } else {
                 output[i] = input[i];
             }
--- src/test/java/mettle/MappingUtilityTest.java
+++ src/test/java/mettle/MappingUtilityTest.java
@@ -3,7 +3,7 @@ package mettle;
 import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.Test;
-import java.util.HashMap;
+import java.util.*;
 
 /**
  * Unit tests for valid input values and valid mappings in the HashMap
@@ -15,12 +15,15 @@ public class MappingUtilityTest {
         // Input: [1, 2, 3, 1, 5]
         // Mapping: {1: 10, 2: 33}
         // Expected Output: [10, 33, 3, 10, 5]
-        int[] input = {1, 2, 3, 1, 5};
-        HashMap<Integer, Integer> m = new HashMap<>();
+        Integer[] input = {1, 2, 3, 1, 5};
+        Map<Integer, Integer> m = new HashMap<>();
         m.put(1, 10);
         m.put(2, 33);
-        int[] expectedOutput = {10, 33, 3, 10, 5};
-        int[] output = MappingUtility.applyMapping(input, m);
+        Integer[] expectedOutput = {10, 33, 3, 10, 5};
+        
+        // Create an instance of MappingUtility with the initial mapping
+        MappingUtility<Integer> mappingUtility = new MappingUtility<>(m);
+        Integer[] output = mappingUtility.applyMapping(input);
         assertArrayEquals(expectedOutput, output);
     }
 
@@ -29,10 +32,13 @@ public class MappingUtilityTest {
         // Input: []
         // Mapping: {1: 10, 2: 33}
         // Expected Output: []
-        int[] input = {};
-        HashMap<Integer, Integer> m = new HashMap<>();
-        int[] expectedOutput = {};
-        int[] output = MappingUtility.applyMapping(input, m);
+        Integer[] input = {};
+        Map<Integer, Integer> m = new HashMap<>();
+        Integer[] expectedOutput = {};
+        
+        // Create an instance of MappingUtility with the initial mapping
+        MappingUtility<Integer> mappingUtility = new MappingUtility<>(m);
+        Integer[] output = mappingUtility.applyMapping(input);
         assertArrayEquals(expectedOutput, output);
     }
 
@@ -41,10 +47,13 @@ public class MappingUtilityTest {
         // Input: [1, 2, 3]
         // Mapping: {}
         // Expected Output: [1, 2, 3]
-        int[] input = {1, 2, 3};
-        HashMap<Integer, Integer> m = new HashMap<>();
-        int[] expectedOutput = {1, 2, 3};
-        int[] output = MappingUtility.applyMapping(input, m);
+        Integer[] input = {1, 2, 3};
+        Map<Integer, Integer> m = new HashMap<>();
+        Integer[] expectedOutput = {1, 2, 3};
+        
+        // Create an instance of MappingUtility with the initial mapping
+        MappingUtility<Integer> mappingUtility = new MappingUtility<>(m);
+        Integer[] output = mappingUtility.applyMapping(input);
         assertArrayEquals(expectedOutput, output);
     }
 
@@ -53,12 +62,15 @@ public class MappingUtilityTest {
         // Input: [1, 2, 3]
         // Mapping: {4: 40, 5: 50}
         // Expected Output: [1, 2, 3]
-        int[] input = {1, 2, 3};
-        HashMap<Integer, Integer> m = new HashMap<>();
+        Integer[] input = {1, 2, 3};
+        Map<Integer, Integer> m = new HashMap<>();
         m.put(4, 40);
         m.put(5, 50);
-        int[] expectedOutput = {1, 2, 3};
-        int[] output = MappingUtility.applyMapping(input, m);
+        Integer[] expectedOutput = {1, 2, 3};
+        
+        // Create an instance of MappingUtility with the initial mapping
+        MappingUtility<Integer> mappingUtility = new MappingUtility<>(m);
+        Integer[] output = mappingUtility.applyMapping(input);
         assertArrayEquals(expectedOutput, output);
     }
 }
